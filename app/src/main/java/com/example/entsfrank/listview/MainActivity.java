a1ad92bbc92f4736bf2a98cc8a9c9483f7cd7516
skafis <franciskipchumba5@gmail.com>
2016-03-06 14:49:33 +0300

    add on click listener and toast

--- app/src/main/java/com/example/entsfrank/listview/MainActivity.java
+++ app/src/main/java/com/example/entsfrank/listview/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.entsfrank.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +18,22 @@ public class MainActivity extends AppCompatActivity {
 
         String[]  skills = {"HTML","CSS","android","python","Django","Java","JavaScript","UX/UI","C++","Application Security SPecialist"};
 
-        ListAdapter theAdapter
-        theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-                skills );
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, skills );
 
+        ListView theListView = (ListView) findViewById(R.id.listview);
+//        tells list view what to do
 
+        theListView.setAdapter(theAdapter);
+
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AdapterView adapterView = null;
+                String skillspicked= "you selected " + String.valueOf(adapterView.getItemAtPosition(position));
+                Toast.makeText(MainActivity. this , skillspicked, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        }
     }
-}
+
